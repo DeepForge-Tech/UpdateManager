e5882c9dd915ebeb0e2ab2802e2a6955d28a8e32
blackflame576 <pws15548@gmail.com>
2023-09-29 23:12:24 +0300

    Refactoring

--- BuildProject.bat
+++ BuildProject.bat
@@ -1,5 +1,14 @@
 @echo off
+if exist .\build (
+    rem file exists
+) else (
+    mkdir build
+)
 echo -- Building library Zipper
+cd src
+if exist .\src\zipper (
+    rmdir .\src\zipper /s /q
+)
 git clone --recursive https://github.com/sebastiandev/zipper.git
 mkdir lib
 cd zipper
@@ -11,6 +20,7 @@ Xcopy *.a  ..\lib\  /Y
 cd ..
 rmdir .\zipper /s /q
 echo - Zipper successfully builded
+cd ..
 echo -- Building UpdateManager
-g++ -o UpdateManager.exe UpdateManager.cpp .\resource.res -I ..\..\include -I .\include\ -L ..\..\lib\  -L .\lib\ -static -static-libgcc -static-libstdc++ -lurlmon -lcurl -lsqlite3 -lws2_32 -lwinmm -ljsoncpp -lZipper -lz -std=c++20 -w
+g++ -o .\build\UpdateManager.exe .\src\UpdateManager.cpp .\src\resource.res -I ..\..\include -I .\src\include\ -L ..\..\lib\  -L .\src\lib\ -static -static-libgcc -static-libstdc++ -lurlmon -lcurl -lsqlite3 -lws2_32 -lwinmm -ljsoncpp -lZipper -lz -std=c++20 -w
 echo -- Build of UpdateManager finished
